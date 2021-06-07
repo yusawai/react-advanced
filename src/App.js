e9a8a1e7c64345f3d47b39ecfc4098f768f56305
yusawai <73582367+yusawai@users.noreply.github.com>
2021-06-07 14:39:11 +0800

    ファイル初期化

--- src/App.js
+++ src/App.js
@@ -1,32 +1,5 @@
-import { useState, useCallback } from "react";
-import { ChildArea } from "./ChildArea";
-import { CssModules } from "./components/CssModules";
-import { Emotion } from "./components/Emotion";
-import { InlineStyle } from "./components/InlineStyle";
-import { StyledComponents } from "./components/StyledComponents";
-import { StyledJsx } from "./components/StyledJsx";
 import "./styles.css";
 
 export default function App() {
-  const [text, setText] = useState("");
-  const [isOpen, setIsOpen] = useState(false);
-  const onClickShowBtn = () => setIsOpen(!isOpen);
-  const onChangeText = (e) => setText(e.target.value);
-  // アロー関数（＝コールバック関数）の再レンダリングを制御する useCallBack()
-  const onClickCloseBtn = useCallback(() => setIsOpen(false), [setIsOpen]);
-
-  return (
-    <div className="App">
-      <input value={text} onChange={onChangeText} />
-      <br />
-      <br />
-      <button onClick={onClickShowBtn}>Show</button>
-      <ChildArea open={isOpen} onClick={onClickCloseBtn} />
-      <InlineStyle />
-      <CssModules />
-      <StyledJsx />
-      <StyledComponents />
-      <Emotion />
-    </div>
-  );
+  return <div className="App"></div>;
 }
