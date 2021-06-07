ef6fecc3c41feade829e5c8c463c368b420b995f
yusawai <73582367+yusawai@users.noreply.github.com>
2021-06-07 14:43:00 +0800

    準備好了

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./styles.css";
 
+/**
+ * 1. react-router-dom を package.json に追加
+ * 2.
+ */
 export default function App() {
   return <div className="App"></div>;
 }
---
+++ src/Page2.jsx
@@ -0,0 +1,7 @@
+export const Page2 = () => {
+  return (
+    <div>
+      <h1>This's Page2.</h1>
+    </div>
+  );
+};
