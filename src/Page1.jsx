4e9e9e5307537cf72a69747c12abcbba60b54439
yusawai <73582367+yusawai@users.noreply.github.com>
2021-06-08 20:49:54 +0800

    Stateによりページ間で値を引き継ぐ Sec5 Lesson24

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()];
+  console.log(arr);
+
   return (
     <div>
       <h1>This's Page1.</h1>
-      <Link to="/page1/detailA">DetailA</Link>
+      <Link to={{ pathname: "/page1/detailA", state: arr }}>DetailA</Link>
       <br />
       <Link to="/page1/detailB">DetailB</Link>
     </div>
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,4 +1,8 @@
+import { useLocation } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const { state } = useLocation();
+
   return (
     <div>
       <h1>This's Page1DetailA.</h1>
